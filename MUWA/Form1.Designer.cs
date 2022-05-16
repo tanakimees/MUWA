c195dedbc322d6a25da2cf90333f0a124425999e
Kristo Tänak <tanak.kristo@gmail.com>
2022-05-16 11:31:37 +0200

    Tee loc ja type

--- MUWA/Form1.Designer.cs
+++ MUWA/Form1.Designer.cs
@@ -2119,8 +2119,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
